f4dd2ae6b92a49171fbe3fa6d02fdd871e2aa2b9
Tomas <tomas.millan96@gmail.com>
2024-08-20 00:29:42 -0300

    Some Fixes and official website launch

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,19 @@ import '../styles/globals.css'
 import Footer from "@/components/Footer";
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import TagManager from 'react-gtm-module';
 config.autoAddCss = false
 
 
 export default function App({ Component, pageProps }) {
   
+  const tagManagerArgs = {
+    gtmId: 'AW-744057013',
+    events: {
+    sendUserInfo: 'userInfo'
+    }
+    }
+    TagManager.initialize(tagManagerArgs)
   return (
     <>
       <CartContextProvider>
